7bb7e08eaad11ea6b7953625f0c7ca178790ddfd
karim_latif <attar4443@gmail.com>
2021-01-27 22:42:40 +0700

    remove ap

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"net/http"
 	"os"
 
 	"github.com/gin-gonic/contrib/static"
@@ -14,16 +13,6 @@ func main() {
 
 	// Serve frontend static files
 	router.Use(static.Serve("/", static.LocalFile("./public", true)))
-
-	// Setup route group for the API
-	api := router.Group("/api")
-	{
-		api.GET("/", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"message": "pong",
-			})
-		})
-	}
 
 	router.NoRoute(func(c *gin.Context) {
 		c.File("./public/index.html")
