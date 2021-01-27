0e951329a840691df0a914dc0dac09a2cf20b825
karim_latif <attar4443@gmail.com>
2021-01-27 22:29:10 +0700

    main gin

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/yakarim/frontend-karim
 
 go 1.15
+
+require (
+	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19
+	github.com/gin-gonic/gin v1.6.3
+)
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	router := gin.Default()
+
+	// Serve frontend static files
+	router.Use(static.Serve("/", static.LocalFile("./public", true)))
+
+	// Setup route group for the API
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+
+	router.NoRoute(func(c *gin.Context) {
+		c.File("./public/index.html")
+	})
+
+	port = "3001"
+	// Start and run the server
+	router.Run(":" + port)
+}
